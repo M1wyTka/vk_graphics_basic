0c5ae0a62452bbc0381f0111346aa70a59344603
M1wyTka <megalagerspb@gmail.com>
2021-11-26 23:21:01 +0300

    Optimization of dragon scene
    Frag shader changes and simple frustrum culling

---
+++ resources/shaders/compile_bad_optimization_shaders.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+import pathlib
+
+if __name__ == '__main__':
+    glslang_cmd = "glslangValidator"
+
+    shader_list = ["simple_bad_optimization.frag", "simple.vert", "culling.comp"]
+
+    for shader in shader_list:
+        subprocess.run([glslang_cmd, "-V", shader, "-o", "{}.spv".format(shader)])
+
